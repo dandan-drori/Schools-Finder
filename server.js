7589366d9af4e2a55cbf760bda269c72e2667c35
Dandan Drori <55210781+dandan-drori@users.noreply.github.com>
2021-09-10 18:49:42 +0300

    check if fileURLToPath can fix deployment issue

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 import express from 'express'
 import cors from 'cors'
 import path from 'path'
+import { fileURLToPath } from 'url'
 const app = express()
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 app.use(express.json())
 app.use(cors())
-const __dirname = path.resolve()
 app.use(express.static(path.join(__dirname, '../client/build')))
+// app.use(express.static('../client/build'))
 
 import enrichRoutes from './api/enrich/enrich.routes.js'
 app.use('/api/enrich', enrichRoutes)

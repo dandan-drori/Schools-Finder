f0484be7b30922feca6102792d8abc25cd459eb1
Dandan Drori <55210781+dandan-drori@users.noreply.github.com>
2021-09-11 13:39:09 +0300

    add universal route to get index.html

--- server.js
+++ server.js
@@ -4,8 +4,20 @@ import path from 'path'
 const app = express()
 
 app.use(express.json())
-app.use(cors())
-app.use(express.static(path.join(process.cwd(), '../client/build')))
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.resolve(process.cwd(), '../client/build')))
+} else {
+	const corsOptions = {
+		origin: ['http://127.0.0.1:3000', 'http://localhost:3000'],
+		credentials: true,
+	}
+	app.use(cors(corsOptions))
+}
+
+app.get('/**', (req, res) => {
+	res.sendFile(path.join(process.cwd(), '../client/build', 'index.html'))
+})
 
 import enrichRoutes from './api/enrich/enrich.routes.js'
 app.use('/api/enrich', enrichRoutes)

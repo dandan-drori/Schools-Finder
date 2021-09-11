ada9c11a361ef3ce9d2c6846409429e8da7e64ee
Dandan Drori <55210781+dandan-drori@users.noreply.github.com>
2021-09-11 13:44:44 +0300

    test online fix to path resolve

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 import express from 'express'
 import cors from 'cors'
 import path from 'path'
+import { fileURLToPath } from 'url'
 const app = express()
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 app.use(express.json())
 
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static(path.join(process.cwd(), '../client/build')))
+	app.use(express.static(path.join(__dirname, '../client/build')))
 } else {
 	const corsOptions = {
 		origin: ['http://127.0.0.1:3000', 'http://localhost:3000'],
@@ -16,7 +19,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.get('/**', (req, res) => {
-	res.sendFile(path.join(process.cwd(), '../client/build', 'index.html'))
+	res.sendFile(path.join(__dirname, '../client/build', 'index.html'))
 })
 
 import enrichRoutes from './api/enrich/enrich.routes.js'

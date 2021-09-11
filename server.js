b6589db40f9d5c0d817c5feaccb581a656291cca
Dandan Drori <55210781+dandan-drori@users.noreply.github.com>
2021-09-11 13:33:55 +0300

    try process.cwd for path

--- server.js
+++ server.js
@@ -1,16 +1,11 @@
 import express from 'express'
 import cors from 'cors'
 import path from 'path'
-import { fileURLToPath } from 'url'
 const app = express()
-
-const filename = fileURLToPath(import.meta.url)
-const dirname = path.dirname(filename)
 
 app.use(express.json())
 app.use(cors())
-// app.use(express.static(path.join(dirname, '../client/build')))
-app.use(express.static('../client/build'))
+app.use(express.static(path.join(process.cwd(), '../client/build')))
 
 import enrichRoutes from './api/enrich/enrich.routes.js'
 app.use('/api/enrich', enrichRoutes)
